57896b8127a948140255bf27c67dac134e006673
cjohnson73 <cjohnson73@murraystate.edu>
2018-09-26 11:31:02 -0400

    IDk porlkschops

---
+++ PlayShop/PlayShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlayShop.WebUI.MvcApplication" Language="C#" %>
---
+++ PlayShop/PlayShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayShop.WebUI.Startup))]
+namespace PlayShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
